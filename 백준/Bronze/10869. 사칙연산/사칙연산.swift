6292510df5ef0557ad565743a1c61c7f347768f5
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-05-13 09:33:58 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,11 @@
+let input = readLine()!
+let arry = input.split(separator: " ")
+
+let a = Int(arry[0])!
+let b = Int(arry[1])!
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
