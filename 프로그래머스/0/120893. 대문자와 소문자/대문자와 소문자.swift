39d8ca86296b34e4dba36867458b11b6daf7e566
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-07-22 17:14:33 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.21 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    
+    var result = ""
+    
+    for i in my_string {
+        
+        if i.isUppercase {
+            result += i.lowercased()
+        } else {
+            result += i.uppercased()
+        }
+    }
+    
+    return result
+}
