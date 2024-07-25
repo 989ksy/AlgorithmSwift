03c8379b3f6193f2e1a14915042d43cc78d8cfd3
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-07-25 09:54:30 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.swift
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.swift
@@ -2,18 +2,4 @@ let a = Int(readLine()!)!
 let b = Int(readLine()!)!
 let c = Int(readLine()!)!
 
-let sum = a + b + c
-
-if sum == 180 {
-    
-    if a == b && b == c {
-        print ("Equilateral")
-    } else if a == b || b == c || a == c {
-        print("Isosceles")
-    } else {
-        print ("Scalene")
-    }
-    
-} else {
-    print("Error")
-}
+print(a + b + c == 180 ? (a == b && b == c ? "Equilateral" : a == b || b == c || a == c ? "Isosceles" : "Scalene" ) : "Error")
