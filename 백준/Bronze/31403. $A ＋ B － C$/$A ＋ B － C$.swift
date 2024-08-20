2d5d5dde91a4c7fe1ad8c07054d4ee08de870e8b
Seungyeon <122261047+989ksy@users.noreply.github.com>
2024-08-20 17:45:18 +0900

    [Bronze IV] Title: $A + B - C$, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.swift
@@ -0,0 +1,9 @@
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+let c = Int(readLine()!)!
+
+let stringAB = String(a) + String(b)
+let stringCal = Int(stringAB)! - c
+
+print(a + b - c)
+print(stringCal)
